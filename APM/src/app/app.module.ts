facc92a24b4d865da26533e11e4f2a4570c11f71
Carmen Domínguez Campillo <>
2018-09-14 21:33:32 +0200

    Templates, Interpolation and Directives

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent //directive used in any component
   ],
   imports: [
     BrowserModule
   ],
   providers: [], //for services
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //first component loaded
 })
 export class AppModule { }
